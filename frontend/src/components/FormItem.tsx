b5c8542ec7b82f24ce754b1cf14f5428b7f0baf9
yubintw <yubin.cs05g@nctu.edu.tw>
2021-07-25 14:44:50 +0800

    add frontend project

---
+++ frontend/src/components/FormItem.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { IForm } from '../types/form'
+
+
+const Form = (
+    props: {
+        form: IForm,
+    }) => {
+
+    return (
+        <tr>
+            <td>{props.form._id}</td>
+            <td>{props.form.computerName}</td>
+            <td>{props.form.status}</td>
+            <td>{props.form.createdAt}</td>
+        </tr>
+    )
+}
+
+export default Form
